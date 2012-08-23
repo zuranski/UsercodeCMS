cd856dc3d3837a29002a9c6b2b0c66f2278d35a6
zuranski <>
2012-08-23 19:12:07 +0000

    test removal

--- DisplacedJetAnlzr/src/classes.h
+++ DisplacedJetAnlzr/src/classes.h
@@ -1,9 +1,17 @@
+#ifndef DJ_CLASSES_H
+#define DJ_CLASSES_H
+
 #include "MyAnalysis/DisplacedJetAnlzr/interface/genjet.h"
 #include "MyAnalysis/DisplacedJetAnlzr/interface/pfjet.h"
 #include "MyAnalysis/DisplacedJetAnlzr/interface/pfjetpair.h"
 #include "MyAnalysis/DisplacedJetAnlzr/interface/exotic.h"
-#include "MyAnalysis/DisplacedJetAnlzr/interface/trgObj.h"
+#include "MyAnalysis/DisplacedJetAnlzr/interface/djcandidate.h"
 #include "DataFormats/Common/interface/Wrapper.h"
+#include <string>
+#include <map>
+#include "Math/LorentzVector.h"
+#include "Math/PtEtaPhiE4D.h"
+#include "Math/PtEtaPhiM4D.h"
 
 namespace {
 
@@ -18,8 +26,21 @@ namespace {
     edm::Wrapper<std::vector<pfjet> > pfjets;
     edm::Wrapper<pfjetpair> pfjpair;
     edm::Wrapper<std::vector<pfjetpair> > pfjetpairs;
-    edm::Wrapper<trgObj> t0;
-    edm::Wrapper<std::vector<trgObj> > t0s;
+    edm::Wrapper<djcandidate> djc;
+    edm::Wrapper<std::vector<djcandidate> > djcs;
+
+    std::map<std::string,bool> dummy0;
+    edm::Wrapper<std::map<std::string,bool> > dummy1;
+
+    std::map<std::string,int> dummi0;
+    edm::Wrapper<std::map<std::string,int> > dummi1;
+
+    std::map<std::string,std::string> dummee0;
+    edm::Wrapper<std::map<std::string,std::string> > dummee1;
+
+    ROOT::Math::PtEtaPhiM4D<float> dumdum;
   };
 
 }
+
+#endif
