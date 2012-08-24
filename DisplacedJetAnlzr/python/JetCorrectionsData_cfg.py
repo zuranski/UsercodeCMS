13f300e39bc53487763dd2cf8339024bfb987235
zuranski <>
2012-08-24 15:33:44 +0000

    remove old files.

--- DisplacedJetAnlzr/python/JetCorrectionsData_cfg.py
+++
@@ -1,7 +0,0 @@
-### Jet Corections  ###
-
-process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
-process.ak5CaloJetsCorrected = process.ak5CaloJetsL1L2L3Residual.clone( src = "CaloJetsTightId" )
-process.ak5PFJetsCorrected = process.ak5PFJetsL1L2L3Residual.clone( src = "PFJetsTightId" )
-
-process.JetCorrectionsSeq = cms.Sequence(process.ak5CaloJetsCorrected * process.ak5PFJetsCorrected)
--- DisplacedJetAnlzr/python/JetCorrectionsMC_cfg.py
+++
@@ -1,8 +0,0 @@
-### Jet Corections  ### as recommended by JEC group
-# https://twiki.cern.ch/twiki/bin/view/CMS/IntroToJEC#Mandatory_Jet_Energy_Corrections
- 
-process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
-process.ak5CaloJetsCorrected = process.ak5CaloJetsL1L2L3.clone( src = "CaloJetsTightId" )
-process.ak5PFJetsCorrected = process.ak5PFJetsL1L2L3.clone( src = "PFJetsTightId" )
-
-process.JetCorrectionsSeq = cms.Sequence(process.ak5CaloJetsCorrected * process.ak5PFJetsCorrected)
