db8680161885fa4d0debf0e6faafec6281c1e646
zuranski <>
2012-11-09 14:35:42 +0000

    adapting triggers and trigger objects for 2012 run.

--- DisplacedJetAnlzr/python/DJ_TriggerObjects_cfi.py
+++ DisplacedJetAnlzr/python/DJ_TriggerObjects_cfi.py
@@ -1,16 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 objects = [
-           'hltHT250', #2011
+           #'hltHT250', #2011
            'hltHT300', #2011check,2012
-	   'hlt2DisplacedHT250L3Filter',#2011 Run A
-           'hlt2DisplacedHT2501PTrkL3Filter',#2011 Run B
-           #'hlt2DisplacedHT300L1FastJetL3Filter',#2012
-           #'hlt1DisplacedHT300L1FastJetL3Filter',#2012
-	   #'hlt2PFDisplacedJetsPt50', #2012
-	   #'hlt1PFDisplacedJetsPt50', #2012
-	   #'hlt2PFDisplacedJetsPt60ChgFraction10', #2012
-	   #'hlt1PFDisplacedJetsPt60ChgFraction10', #2012
+	   #'hlt2DisplacedHT250L3Filter',#2011 Run A
+           #'hlt2DisplacedHT2501PTrkL3Filter',#2011 Run B
+           'hlt2DisplacedHT300L1FastJetL3Filter',#2012
+           'hlt1DisplacedHT300L1FastJetL3Filter',#2012
+	   'hlt2PFDisplacedJetsPt50', #2012
+	   'hlt1PFDisplacedJetsPt50', #2012
+	   'hlt2PFDisplacedJetsPt60ChgFraction10', #2012
+	   'hlt1PFDisplacedJetsPt60ChgFraction10', #2012
 ]
 
 djtriggerobjects= cms.EDProducer("DJ_TriggerObjects",
