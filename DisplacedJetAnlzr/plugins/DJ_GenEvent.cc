deac178e59947dc993084e2d34997e37d1954e16
zuranski <>
2013-04-19 22:45:00 +0000

    added mixed signal decays to ntuples

--- DisplacedJetAnlzr/plugins/DJ_GenEvent.cc
+++ DisplacedJetAnlzr/plugins/DJ_GenEvent.cc
@@ -2,6 +2,10 @@
 
 DJ_GenEvent::DJ_GenEvent(const edm::ParameterSet& iConfig) {
   produces <std::vector<int> >         ( "XpdgId"  );
+  produces <std::vector<float> >         ( "XPt"  );
+  produces <std::vector<float> >         ( "XPhi"  );
+  produces <std::vector<float> >         ( "XEta"  );
+  produces <std::vector<float> >         ( "XMass"  );
   produces <std::vector<int> >         ( "genqFlavor" );
   produces <std::vector<float> >         ( "genqPt"  );
   produces <std::vector<float> >         ( "genqEta"  );
@@ -20,12 +24,19 @@ DJ_GenEvent::DJ_GenEvent(const edm::ParameterSet& iConfig) {
   produces <std::vector<float> >         ( "genjetCtau2"  );
   produces <float>          ( "genHT"  );
   produces <float>          ( "HPt"  );
+  produces <float>          ( "HPhi"  );
+  produces <float>          ( "HEta"  );
+  produces <float>          ( "HMass"  );
 }
 
 void DJ_GenEvent::
 produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
   std::auto_ptr<std::vector<int> > XpdgId ( new std::vector<int> );
+  std::auto_ptr<std::vector<float> > XPt ( new std::vector<float> );
+  std::auto_ptr<std::vector<float> > XPhi ( new std::vector<float> );
+  std::auto_ptr<std::vector<float> > XEta ( new std::vector<float> );
+  std::auto_ptr<std::vector<float> > XMass ( new std::vector<float> );
   std::auto_ptr<std::vector<int> > genqFlavor ( new std::vector<int> );
   std::auto_ptr<std::vector<float> > genqPt ( new std::vector<float> );
   std::auto_ptr<std::vector<float> > genqEta ( new std::vector<float> );
@@ -44,6 +55,9 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   std::auto_ptr<std::vector<float> > genjetCtau2 ( new std::vector<float> );
   std::auto_ptr<float> genHT ( new float() );
   std::auto_ptr<float> HPt ( new float() );
+  std::auto_ptr<float> HPhi ( new float() );
+  std::auto_ptr<float> HEta ( new float() );
+  std::auto_ptr<float> HMass ( new float() );
 
   try {
 
@@ -57,16 +71,27 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
       if (p->status()!=3) continue; //not decaying particles
 
       int pdgId=abs(p->pdgId());
-      if (pdgId==35) *HPt.get()=p->pt();
-      if (pdgId!=6001114 && pdgId!=6002114 && pdgId!=6003114) continue;
+      if (pdgId==35) {
+        *HPt.get()=p->pt();
+        *HPhi.get()=p->phi();
+        *HEta.get()=p->eta();
+        *HMass.get()=p->mass();
+      }
+      // qq 
+      if (pdgId!=6001114 && pdgId!=6002114 && pdgId!=6003114      // emu
+      && pdgId!=6001113 && pdgId!=6002113 && pdgId!=6003113) continue;
       exo.push_back(p.get());
       XpdgId->push_back(pdgId);
+      XPt->push_back(p->pt());
+      XPhi->push_back(p->phi());
+      XEta->push_back(p->eta());
+      XMass->push_back(p->mass());
       //std::cout << "gen quarks" << std::endl;
 
       unsigned int nDau = p->numberOfDaughters();
       for (unsigned int i = 0; i < nDau; i++) {
         reco::GenParticleRef dau = p->daughterRef(i);
-        if (abs(dau->pdgId()>5)) continue;
+        if (abs(dau->pdgId()>13)) continue;
         float lxy = (dau->daughterRef(0)->vertex()-dau->vertex()).Rho();
         float lxyz = (dau->daughterRef(0)->vertex()-dau->vertex()).R();
         genqFlavor->push_back(abs(dau->pdgId()));
@@ -103,23 +128,38 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
         if (fabs(j->eta())>2) continue;
 
         //std::cout << "===============================" << std::endl;
-        //std::cout << j->pt() <<  " " << j->eta() << " " << j->phi() << std::endl; 
+        //std::cout << j->pt() <<  " " << j->eta() << " " << j->phi() << " " << j->getGenConstituents().size() << std::endl; 
+
+	// check if that's a hadronic jet
+	int nconst=0;
+        std::vector<const reco::GenParticle*> constituents = j->getGenConstituents();
+        // that's a lepton
+        if (constituents.at(0)->pt() > 0.5*j->pt()) continue;
+        for (unsigned j = 0; j<constituents.size(); j++){
+          const reco::GenParticle* p=constituents.at(j);
+	  if (p->pt()<1) continue;
+	  nconst+=1;
+        }
 
         float exo1Frac=0;
         float exo2Frac=0;
         float pFrac=0;
+	float totPt=0;
 
-        std::vector<const reco::GenParticle*> constituents = j->getGenConstituents();
         for (unsigned j = 0; j<constituents.size(); j++){
           const reco::GenParticle* p=constituents.at(j);
+	  if (p->pt()<1) continue;
+	  //std::cout << p->pdgId() << " " << p->pt() << std::endl;
           const reco::Candidate* mom=deepMother(p);
+          totPt+=p->pt();
           if (mom==exo1) exo1Frac+=p->pt();
           else if (mom==exo2) exo2Frac+=p->pt();
           else pFrac+=p->pt();
         }
-        exo1Frac/=j->pt();
-        exo2Frac/=j->pt();
-        pFrac/=j->pt();
+        exo1Frac/=totPt;
+        exo2Frac/=totPt;
+        pFrac/=totPt;
+	//std::cout << XpdgId->at(0) << " " << XpdgId->at(1) << std::endl;
         //std::cout << exo1Frac << " " << exo2Frac << " " << pFrac << std::endl;
       
         if (exo1Frac>0.5){
@@ -143,6 +183,10 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   }
 
   iEvent.put( XpdgId,   "XpdgId"   );
+  iEvent.put( XPt,   "XPt"   );
+  iEvent.put( XPhi,   "XPhi"   );
+  iEvent.put( XEta,   "XEta"   );
+  iEvent.put( XMass,   "XMass"   );
   iEvent.put( genqFlavor,   "genqFlavor"   );
   iEvent.put( genqPt,   "genqPt"   );
   iEvent.put( genqEta,   "genqEta"   );
@@ -161,6 +205,9 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   iEvent.put( genjetCtau2,   "genjetCtau2"   );
   iEvent.put( genHT,   "genHT"   );
   iEvent.put( HPt,   "HPt"   );
+  iEvent.put( HEta,   "HEta"   );
+  iEvent.put( HPhi,   "HPhi"   );
+  iEvent.put( HMass,   "HMass"   );
 
 }
 
--- DisplacedJetAnlzr/python/DJ_SignalFilter_cfi.py
+++ DisplacedJetAnlzr/python/DJ_SignalFilter_cfi.py
@@ -4,7 +4,9 @@ import GeneratorInterface.GenFilters.XtoFFbarFilter_cfi
 djsignalfilter = GeneratorInterface.GenFilters.XtoFFbarFilter_cfi.XtoFFbarFilter.clone(
    # exotic -> uds
    idMotherX = cms.vint32(6001114,6002114,6003114),
-   idMotherY = cms.vint32(6001114,6002114,6003114),
+#   idMotherY = cms.vint32(6001114,6002114,6003114),
+   idMotherY = cms.vint32(),
    idDaugtherX = cms.vint32(1,2,3,4,5),
-   idDaugtherY = cms.vint32(1,2,3,4,5)
+#   idDaugtherY = cms.vint32(1,2,3,4,5)
+   idDaugtherY = cms.vint32()
 )
